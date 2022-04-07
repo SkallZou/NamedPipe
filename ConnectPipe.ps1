a5b7d39199e964a340e12baa68c42deafecf7835
Olivier LEUNG <olivierleung@hotmail.fr>
2022-04-07 10:08:49 +0200

    Add files via upload

---
+++ ConnectPipe.ps1
@@ -0,0 +1,10 @@
+﻿$pipeName = "YahudPipe"
+$pipe = New-Object System.IO.Pipes.NamedPipeClientStream($pipeName)
+$pipe.Connect();
+
+$sw = New-Object System.IO.StreamWriter($pipe);
+$msg = Read-Host "Enter your message to send";
+$sw.WriteLine($msg)
+
+$sw.Dispose()
+$pipe.Dispose()
---
+++ CreatePipe.ps1
@@ -0,0 +1,16 @@
+﻿try{
+$pipeName = "YahudPipe";
+$pipe = New-Object System.IO.Pipes.NamedPipeServerStream($pipeName); # Creation new pipe object
+Write-Host "Listening on \\.pipe\$pipeName";
+$pipe.WaitForConnection();
+$sr = New-Object System.IO.StreamReader($pipe) # StreamReader read text from a file, from $pipe here
+$msg = $sr.ReadLine();
+Write-Host "Message received :", $msg;
+$sr.Dispose();
+$pipe.Dispose();
+}
+catch{
+Write-Host "Failed to create the pipe $pipeName"
+$_
+return 0;
+}
