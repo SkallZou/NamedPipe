111abd9c25ea215318b42a64dcc1e22fe7b0a07e
Olivier LEUNG <olivierleung@hotmail.fr>
2022-04-08 11:33:39 +0200

    Add files via upload
    Interactive session

--- ConnectPipe.ps1
+++ ConnectPipe.ps1
@@ -1,10 +1,15 @@
 ﻿$pipeName = "YahudPipe"
 $pipe = New-Object System.IO.Pipes.NamedPipeClientStream($pipeName)
 $pipe.Connect();
-
 $sw = New-Object System.IO.StreamWriter($pipe);
+$sw.WriteLine("Olivier")
+$sw.AutoFlush = $true # Without autoflush, buffer will flush when the pipe is close, autoflush is necessary for interactive session
+
+while($pipe.IsConnected){
 $msg = Read-Host "Enter your message to send";
 $sw.WriteLine($msg)
-
-$sw.Dispose()
-$pipe.Dispose()
+    if($msg -eq "exit"){
+        $sw.Dispose()
+        $pipe.Dispose()
+    }
+}
--- CreatePipe.ps1
+++ CreatePipe.ps1
@@ -1,16 +1,29 @@
-﻿try{
-$pipeName = "YahudPipe";
-$pipe = New-Object System.IO.Pipes.NamedPipeServerStream($pipeName); # Creation new pipe object
-Write-Host "Listening on \\.pipe\$pipeName";
-$pipe.WaitForConnection();
-$sr = New-Object System.IO.StreamReader($pipe) # StreamReader read text from a file, from $pipe here
-$msg = $sr.ReadLine();
-Write-Host "Message received :", $msg;
-$sr.Dispose();
-$pipe.Dispose();
+﻿function ReadMsg([System.IO.StreamReader]$StreamReader){
+    $msg = $StreamReader.ReadLine()
+    Write-Host $msg
+}
+
+
+try{
+    $pipeName = "YahudPipe";
+    $pipe = New-Object System.IO.Pipes.NamedPipeServerStream($pipeName); # Creation new pipe object
+    Write-Host "Listening on \\.pipe\$pipeName";
+    $pipe.WaitForConnection();
+    $sr = New-Object System.IO.StreamReader($pipe) # StreamReader read text from a file, from $pipe here
+    $user = $sr.ReadLine()
+    Write-Host "Connection Established... from", $user
+
+
+
+    while ($pipe.IsConnected){
+        ReadMsg($sr)
+    }
+
+    $sr.Dispose();
+    $pipe.Dispose();
+
 }
 catch{
-Write-Host "Failed to create the pipe $pipeName"
-$_
-return 0;
+    Write-Host "Failed to create the pipe $pipeName"
+    return 0;
 }
