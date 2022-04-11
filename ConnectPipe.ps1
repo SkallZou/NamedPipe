317307fe1fec5f91797c20523850b37d834c9149
Olivier LEUNG <olivierleung@hotmail.fr>
2022-04-11 11:39:29 +0200

    Add files via upload

--- ConnectPipe.ps1
+++ ConnectPipe.ps1
@@ -1,15 +1,24 @@
-﻿$pipeName = "YahudPipe"
-$pipe = New-Object System.IO.Pipes.NamedPipeClientStream($pipeName)
-$pipe.Connect();
-$sw = New-Object System.IO.StreamWriter($pipe);
+﻿function ReadMsg([System.IO.StreamReader]$StreamReader){
+    $msg = $StreamReader.ReadLine()
+    if($msg -ne "stop"){
+        Write-Host $msg
+    }
+}
+
+
+$pipeName = "YahudPipe"
+$pipeClient = New-Object System.IO.Pipes.NamedPipeClientStream($pipeName)
+Write-Host "Attempting to connect to the pipe..."
+$pipeClient.Connect();
+Write-Host "Connected."
+$sw = New-Object System.IO.StreamWriter($pipeClient)
+$sr = New-Object System.IO.StreamReader($pipeClient)
 $sw.WriteLine("Olivier")
 $sw.AutoFlush = $true # Without autoflush, buffer will flush when the pipe is close, autoflush is necessary for interactive session
 
-while($pipe.IsConnected){
-$msg = Read-Host "Enter your message to send";
-$sw.WriteLine($msg)
-    if($msg -eq "exit"){
-        $sw.Dispose()
-        $pipe.Dispose()
-    }
+while($pipeClient.IsConnected){
+    ReadMsg($sr)
 }
+
+$sr.Dispose();
+$pipeClient.Dispose();
--- CreatePipe.ps1
+++ CreatePipe.ps1
@@ -3,26 +3,52 @@
     Write-Host $msg
 }
 
-
 try{
     $pipeName = "YahudPipe";
-    $pipe = New-Object System.IO.Pipes.NamedPipeServerStream($pipeName); # Creation new pipe object
+    $pipeServer = New-Object System.IO.Pipes.NamedPipeServerStream($pipeName); # Creation new pipe object
     Write-Host "Listening on \\.pipe\$pipeName";
-    $pipe.WaitForConnection();
-    $sr = New-Object System.IO.StreamReader($pipe) # StreamReader read text from a file, from $pipe here
+    $pipeServer.WaitForConnection();
+    $sr = New-Object System.IO.StreamReader($pipeServer) # StreamReader read text from a file, from $pipe here
+    $sw = New-Object System.IO.StreamWriter($pipeServer)
     $user = $sr.ReadLine()
     Write-Host "Connection Established... from", $user
 
+    $sw.AutoFlush=$true
 
-
-    while ($pipe.IsConnected){
-        ReadMsg($sr)
+    while ($pipeServer.IsConnected){
+        $action = Read-Host "What action do you want to do ?
+        1. Run Malicious code
+        2. Send message to client
+        3. Quit
+        "
+        
+        if ($action -eq 1){
+            Write-Host "Uploading malicious payload to client"
+            Add-Type -outputtype consoleapplication -outputassembly helloworld.exe 'public class helloworld{public static void Main(){System.Console.WriteLine("Hello World !");}}'
+        }
+        elseif ($action -eq 2){
+            while($msg -ne "stop"){
+                $msg = Read-Host "Enter your message to send"
+                $sw.WriteLine($msg)
+            }
+        }
+        else{
+            if(Test-Path -Path .\helloworld.exe -PathType Leaf){
+                Write-Host "File exist, deleting..."
+                $command = 'del .\helloworld.exe'
+                Invoke-Expression $command
+            }
+            $sw.WriteLine("Disconnected.")
+            $sw.Dispose()
+            $pipeServer.Dispose()
+        }
     }
 
     $sr.Dispose();
-    $pipe.Dispose();
+    $pipeServer.Dispose();
 
 }
+
 catch{
     Write-Host "Failed to create the pipe $pipeName"
     return 0;
